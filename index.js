a55ac819c94b3fda1561b39ea97683aab1cd8910
unknown <umeshbilagi@gmail.com>
2019-11-29 12:37:56 +0530

    version 2

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 "use strict"
-let titleCase = (str)=>{
+let titleCase = (str, restToLowerCase=false)=>{
     let strArray =str.split( " ");
 
     let retStr = "";
+    if(restToLowerCase === true){
+        strArray.forEach(word=>{
+            if(word !== ""){
+                let concatWord = `${word.charAt(0).toUpperCase()}${word.substr(1).toLowerCase()} `
+                retStr= retStr.concat(concatWord);
+               
+            }
+            
+        })
+    }else{
+        
     strArray.forEach(word=>{
         if(word !== ""){
-            let concatWord = `${word.charAt(0).toUpperCase()}${word.substr(1).toLowerCase()} `
+            let concatWord = `${word.charAt(0).toUpperCase()}${word.substr(1)} `
             retStr= retStr.concat(concatWord);
            
         }
         
     })
+    }
+
+
     
     return retStr.trim();
     
---
+++ test.js
@@ -0,0 +1,5 @@
+const titileCase = require("./index");
+
+let str =  `My ABO blood Group is O positive`
+
+console.log(titileCase(str))
